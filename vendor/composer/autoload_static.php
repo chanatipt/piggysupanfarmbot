632dafaa41080c362c648347385b00e8119132e9
unknown <chanatipt@gmail.com>
2022-10-30 05:40:06 +0700

    first commit

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,16 @@ class ComposerStaticInitea0390d6cea695588c7a75c7618ffacb
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitea0390d6cea695588c7a75c7618ffacb::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitea0390d6cea695588c7a75c7618ffacb::$prefixDirsPsr4;
+            $loader->classMap = ComposerStaticInitea0390d6cea695588c7a75c7618ffacb::$classMap;
 
         }, null, ClassLoader::class);
     }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '85f82d0ea1a7db163b442ef465602371b83ba870',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '85f82d0ea1a7db163b442ef465602371b83ba870',
+            'dev_requirement' => false,
+        ),
+        'linecorp/line-bot-sdk' => array(
+            'pretty_version' => '2.3.0',
+            'version' => '2.3.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../linecorp/line-bot-sdk',
+            'aliases' => array(),
+            'reference' => '38528aaa3a1abc2809974ab65cdcb7579f9eb927',
+            'dev_requirement' => false,
+        ),
+    ),
+);
